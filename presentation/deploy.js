f3762f1f727080071f6269ab376da2aa1c14183b
jjagwe dennis <dennisjjagwe@gmail.com>
2018-10-21 21:00:03 +0300

    Setup presentation (#1)
    * Setup Presentation
    
    * Customize setup
    
    * Add speaker notes
    
    * Add script to deploy to github

---
+++ presentation/deploy.js
@@ -0,0 +1,15 @@
+const ghp = require('gh-pages');
+const shell = require('shelljs');
+
+shell.echo('Building the slides....');
+const build = shell.exec(`yarn build`);
+
+if (build.code === 0) {
+  shell.echo('Finished Building');
+  shell.echo('Publishing to gh-pages.....');
+  ghp.publish('dist', () => {
+    console.log('Finished Building....');
+  });
+} else {
+  shell.echo(`Building Failed with code${build.code}`);
+}
