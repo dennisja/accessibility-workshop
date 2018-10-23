c04a0a4cb1ee57bf8849ab1fda804dc34d354e6f
jjagwe dennis <dennisjjagwe@gmail.com>
2018-10-23 22:34:31 +0300

    Add focus slides (#3)

--- presentation/index.js
+++ presentation/index.js
@@ -1,6 +1,7 @@
 import speaker from './slides/speaker.mdx';
 import accessibilityIntroSlides from './slides/accessibility.mdx';
+import focusSlides from './slides/focus.mdx';
 
 export { default as theme } from './theme';
 
-export default [...speaker, ...accessibilityIntroSlides];
+export default [...speaker, ...accessibilityIntroSlides, ...focusSlides];
