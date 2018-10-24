35210bfb75667b157da016a054d8c1179146ee51
dennisja <dennisjjagwe@gmail.com>
2018-10-24 13:11:40 +0300

    Add semantics slides

--- presentation/index.js
+++ presentation/index.js
@@ -1,7 +1,13 @@
 import speaker from './slides/speaker.mdx';
 import accessibilityIntroSlides from './slides/accessibility.mdx';
 import focusSlides from './slides/focus.mdx';
+import semanticsSlides from './slides/semantics.mdx';
 
 export { default as theme } from './theme';
 
-export default [...speaker, ...accessibilityIntroSlides, ...focusSlides];
+export default [
+  ...speaker,
+  ...accessibilityIntroSlides,
+  ...focusSlides,
+  ...semanticsSlides,
+];
